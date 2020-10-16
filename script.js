03a3d64a2645f9110bcae2c2f823cb4224c2ebca
Eduardo Pinheiro <72766650+odraudep@users.noreply.github.com>
2020-10-16 18:29:22 -0300

    add script

---
+++ script.js
@@ -0,0 +1,14 @@
+let sky = document.querySelector('#sky')
+let bmount = document.querySelector('#bmount')
+let fmount = document.querySelector('#fmount')
+let txt = document.querySelector('#txt')
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY 
+    console.log(value) 
+
+    sky.style.top = value * 0.15 + 'px'
+    bmount.style.bottom = -value * 0.35 + 'px'
+    fmount.style.bottom = -value * 0.15 + 'px'
+    txt.style.top = value + 'px'
+})
