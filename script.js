9bae0b0444fb722b97f264e9a169243386e0ca1c
Eduardo Pinheiro <72766650+odraudep@users.noreply.github.com>
2020-10-18 01:28:13 -0300

    add footer

--- script.js
+++ script.js
@@ -3,18 +3,17 @@ let sky = document.querySelector('#sky')
 let bmount = document.querySelector('#bmount')
 let fmount = document.querySelector('#fmount')
 let txt = document.querySelector('#txt')
-// octocat var
+// myoctocat var
 let div_img = document.querySelector('#img')
 
+// efeito scroll
 window.addEventListener('scroll', function() {
     let value = window.scrollY 
-    // console.log(value) 
 
     sky.style.top = value * 0.15 + 'px'
     bmount.style.bottom = -value * 0.35 + 'px'
     fmount.style.bottom = -value * 0.15 + 'px'
     txt.style.top = value + 'px'
-
     // section script
     let area = document.querySelector('.area')
 
